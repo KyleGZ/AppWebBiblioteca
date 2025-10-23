b7763aaee07f01b34ccf5e7024d8c5e644351604
Gdiazm14 <163466647+Gdiazm14@users.noreply.github.com>
2025-10-22 22:30:30 -0600

    Se realizaron los cambios para cargar los datos de la api

--- Models/LibroListaView.cs
+++ Models/LibroListaView.cs
@@ -5,9 +5,9 @@
         public int IdLibro { get; set; }
         public string Titulo { get; set; }
         public string ISBN { get; set; }
-        public string Autor { get; set; }
+        public List<string> Autor { get; set; } = new();
         public string Editorial { get; set; }
-        public string Genero { get; set; }
+        public List<string> Genero { get; set; } = new();
         public string Estado { get; set; }
         public string PortadaUrl { get; set; }
     }
