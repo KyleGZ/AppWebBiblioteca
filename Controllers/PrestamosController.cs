ac9cfb9cdf617f07c38db9e0dd425afb49d6a0ed
alerm <alerm@PC-ALE>
2025-11-01 22:15:00 -0600

    Integracion Modulo Prestamos

---
+++ Controllers/PrestamosController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppWebBiblioteca.Controllers
+{
+    [Authorize] // Opcional: restringe a usuarios autenticados
+    public class PrestamosController : Controller
+    {
+        // GET: /Prestamos
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Préstamos y Devoluciones";
+            ViewData["PageTitle"] = "Préstamos y Devoluciones";
+            return View();
+        }
+    }
+}
+
