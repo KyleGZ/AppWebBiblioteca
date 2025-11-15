0415453f2fe93b69f6ae07d2e39db65233998d23
JorgeSolis7 <solisrojasjorge@gmail.com>
2025-11-14 23:04:18 -0600

    Modulo Gestion bibliografico terminado.

--- Controllers/GestionBibliograficaController.cs
+++ Controllers/GestionBibliograficaController.cs
@@ -1,13 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AppWebBiblioteca.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AppWebBiblioteca.Controllers
 {
     public class GestionBibliograficaController : Controller
     {
+        private readonly IAutorService _autorService;
+        private readonly IAuthService _authService;
+
+        public GestionBibliograficaController(IAuthService authorizacion)
+        {
+            _authService = authorizacion;
+        }
 
         [HttpGet]
         public IActionResult Index()
         {
+            if (!_authService.IsAuthenticated())
+                return RedirectToAction("Login", "Usuario");
             return View();
         }
     }
